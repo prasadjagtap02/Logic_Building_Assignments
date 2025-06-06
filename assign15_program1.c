b0d89fda6387434fb4d49de86f968ce232b69398
prasadjagtap02 <prasad.jagtap7@gmail.com>
2025-06-06 12:55:16 +0530

    Assignment_15

---
+++ assign15_program1.c
@@ -0,0 +1,57 @@
+// Accept number of rows and number of columns from user and display below pattern.
+/*
+Input : iRow = 4    iCol = 4
+
+Output :
+*   #   #   #
+*   *   #   #
+*   *   *   #
+*   *   *   *
+
+*/
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    
+    if(iRow == iCol)
+    {
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i >= j)
+                {
+                    printf("*\t");
+                }
+                else
+                {
+                    printf("#\t");
+                }
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("\n");
+        printf("Invalied Input! Please enter same value for Rows and Columns.");
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
